7d7dd80d4ff4ffea386ef4a081e40c908e92af04
Lousher <779546225@qq.com>
2025-09-04 14:15:59 +0800

    [playground] play raylib codepoints selector

--- playground/raylib/chinese.raylib.macos.c
+++ playground/raylib/chinese.raylib.macos.c
@@ -9,10 +9,15 @@ int main(void) {
   int fileSize;
   unsigned char *fontFileData = LoadFileData("Circle.otf", &fileSize);
 
-  char* text = "这是一条测试数据？";
+  char* text = "一条这是测试数据？纪念抗战胜利！";
+
+  char* text2 = "纪念抗战胜利";
 
   int codePointsCount;
   int *codePoints = LoadCodepoints(text, &codePointsCount);
+
+  int codePointsCount2;
+  int *codePoints2 = LoadCodepoints(text2, &codePointsCount2);
 
   InitWindow(screenWidth, screenHeight, title);
 
@@ -23,6 +28,10 @@ int main(void) {
     300, 200
   };
 
+  Vector2 textPosition2 = {
+    400, 100
+  };
+
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
@@ -30,12 +39,13 @@ int main(void) {
     ClearBackground((Color){135, 206, 235, 255});
         DrawCircle(screenWidth/2, screenHeight/2, 50, GREEN);
         
-        DrawTextEx(myFont, text, textPosition, 50, 0, DARKBLUE);
+        DrawTextEx(myFont, "这是一个测试", textPosition, 50, 0, DARKBLUE);
+        DrawTextEx(myFont, "纪念抗战胜利！", textPosition2, 50, 0, MAROON);    
 
-        DrawText("成功安装", 340, screenHeight/2 - 80, 40, MAROON);
-        DrawText("按ESC或关闭窗口退出", 260, 500, 20, DARKGRAY);
+//        DrawText("成功安装", 340, screenHeight/2 - 80, 40, MAROON);
+//        DrawText("按ESC或关闭窗口退出", 260, 500, 20, DARKGRAY);
         
-        DrawText(TextFormat("FPS: %d", GetFPS()), 10, 10, 20, RED);
+//        DrawText(TextFormat("FPS: %d", GetFPS()), 10, 10, 20, RED);
         
         EndDrawing();
   }
