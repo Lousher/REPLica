ce40e02bf314ab774cf86ba7a3d57048ac2368d0
Lousher <779546225@qq.com>
2026-01-04 17:44:06 +0800

    [Windows Buinding WIP]

---
+++ launcher.windows.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "scheme.h"
+
+int main(int argc, char* argv[]) {
+
+  Sscheme_init(0);
+  Sregister_boot_file("booter\\windows\\petite.boot");
+  Sregister_boot_file("booter\\windows\\scheme.boot");
+
+  Sbuild_heap(argv[0], 0);
+
+  Sscheme_program("replica.boot", argc, (const char **)argv);
+
+  Sscheme_deinit();
+  return 0;
+}
